3ff5362619081b760dac92415068aa32ae8b5977
Hema123kamadolli <hema.kamadolli@mphasis.com>
2024-01-10 12:08:17 +0530

    Insertion

--- obj/Debug/net6.0/ConsAppTable5.AssemblyInfo.cs
+++ obj/Debug/net6.0/ConsAppTable5.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsAppTable5")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a1df1003d10c77008c6e6205979f580d9fda7c8f")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsAppTable5")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsAppTable5")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
